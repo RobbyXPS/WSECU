fda4d49dd227de1cc28ffd2b496a142d758335ca
Robert East <RobbyXPS@gmail.com>
2019-10-25 14:00:44 -0700

    inital sample test is working

---
+++ google_test_async.js
@@ -0,0 +1,14 @@
+const { Builder, By, Key, until } = require("selenium-webdriver");
+
+async function main() {
+  let driver = await new Builder().forBrowser("firefox").build();
+
+  await driver.get("http://www.google.com/ncr");
+
+  const element = await driver.findElement(By.name("q"));
+
+  await element.sendKeys("webdriver", Key.RETURN);
+  await driver.wait(until.titleIs("webdriver - Google Search"), 1000);
+  await driver.quit();
+}
+main();
